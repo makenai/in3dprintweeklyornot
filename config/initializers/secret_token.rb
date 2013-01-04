09fcecf5781b0486aac1ffa3564d96fdb3d5d99e
Pawel Szymczykowski <makenai@gmail.com>
2013-01-03 22:33:06 -0800

    New repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+In3dprintweeklyornot::Application.config.secret_token = '961f4fb6c1e8cfd3bcb285c5a0c223436a341b46b94edbb7a75001ccb67197dc00aa98f6d179780cc344722b16490a12dde2c8ce9bb33f9a2e6067d1890cfc84'
